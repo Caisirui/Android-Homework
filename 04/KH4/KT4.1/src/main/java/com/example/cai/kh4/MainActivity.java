339a359256b2c1d376362eb8a233c70260f3063d
Caisirui <caisirui@126.com>
2016-01-11 23:03:22 +0800

    KT4.1的三种方法都完成，当然右端的小BUG还没解决，先不管了

--- 04/KH4/KT4.1/src/main/java/com/example/cai/kh4/MainActivity.java
+++ 04/KH4/KT4.1/src/main/java/com/example/cai/kh4/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.cai.kh4;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -22,16 +23,52 @@ public class MainActivity extends AppCompatActivity {
     private int direction=2;
     private Button mbutton;
     private boolean On=false;
-    private int width;// = wm.getDefaultDisplay().getWidth();
+    private int posLeft,posRight;
+    private int width;
     DisplayMetrics dm = new DisplayMetrics();
+
+    private Thread thread;
+
     private Handler handler = new Handler(){
       public void handleMessage(Message msg){
           if(msg.what==1) {text.setPadding(text.getPaddingLeft()-10,text.getPaddingTop(),text.getPaddingRight(),text.getPaddingBottom());}
           if(msg.what==2) {text.setPadding(text.getPaddingLeft()+10,text.getPaddingTop(),text.getPaddingRight(),text.getPaddingBottom());}
       }
     };
-    private Thread thread;
 
+    private class RollTask extends AsyncTask<Integer,Integer,Void>{
+
+        protected Void doInBackground(Integer... args){
+            try{
+                while (On) {
+                    Thread.sleep(100);
+                    if (posLeft < width && direction==2) {
+                        // message.what = 2;
+                    }
+                    else if(posRight==0 && direction == 2){
+                        //message.what = 1;
+                        direction = 1;
+                    }
+                    else if(posLeft == 0 && direction == 1){
+                        //message.what = 2;
+                        direction = 2;
+                    }
+                    else if(posLeft < width && direction == 1)
+                    {
+                        // message.what=1;
+                    }
+                    publishProgress();
+            }
+            }catch (InterruptedException e)
+            {Thread.currentThread().interrupt();}
+
+            return null;
+        }
+        protected void onProgressUpdate(Integer...args){
+            if(direction==1) {text.setPadding(text.getPaddingLeft()-10,text.getPaddingTop(),text.getPaddingRight(),text.getPaddingBottom()); posLeft=text.getPaddingLeft(); posRight=text.getPaddingRight();}
+            if(direction==2) {text.setPadding(text.getPaddingLeft()+10,text.getPaddingTop(),text.getPaddingRight(),text.getPaddingBottom()); posLeft=text.getPaddingLeft(); posRight=text.getPaddingRight();}
+        }
+    }
 
 
     @Override
@@ -41,79 +78,102 @@ public class MainActivity extends AppCompatActivity {
         WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
         wm.getDefaultDisplay().getMetrics(dm);
         width = dm.widthPixels;
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
-
-       /*
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
         text = (TextView) findViewById(R.id.text1);
+
         mbutton = (Button) findViewById(R.id.bt);
         mbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(On){thread.interrupt(); On = false;}
-                else{thread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-
-                            while (true) {
-                                Message message = new Message();
-                                Thread.sleep(100);
-                                if (text.getPaddingLeft() < width && direction==2) {
-                                    message.what = 2;
-                                }
-                                else if(text.getPaddingRight() == 0 && direction == 2){
-                                    message.what = 1;
-                                    direction = 1;
-                                }
-                                else if(text.getPaddingLeft() == 0 && direction == 1){
-                                    message.what = 2;
-                                    direction = 2;
-                                }
-                                else if(text.getPaddingLeft() < width && direction == 1)
-                                {
-                                    message.what=1;
-                                }
-                                handler.sendMessage(message);
-                            }
-                        }catch(InterruptedException e){Thread.currentThread().interrupt();}
-                    }
-                });
-                    thread.start();
-                    On = true;}
+                //roll1();
+                //roll2();
+                roll3();
             }
         });
     }
 
+    private void roll1(){
+        if(On){thread.interrupt(); On = false;}
+        else{thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
 
-    /*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+                    while (true) {
+                        Message message = new Message();
+                        Thread.sleep(100);
+                        if (text.getPaddingLeft() < width && direction==2) {
+                            message.what = 2;
+                        }
+                        else if(text.getPaddingRight() == 0 && direction == 2){
+                            message.what = 1;
+                            direction = 1;
+                        }
+                        else if(text.getPaddingLeft() == 0 && direction == 1){
+                            message.what = 2;
+                            direction = 2;
+                        }
+                        else if(text.getPaddingLeft() < width && direction == 1)
+                        {
+                            message.what=1;
+                        }
+                        handler.sendMessage(message);
+                    }
+                }catch(InterruptedException e){Thread.currentThread().interrupt();}
+            }
+        });
+            thread.start();
+            On = true;}
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void roll2(){
+        if(On){thread.interrupt(); On = false;}
+        else{thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+                    while (true) {
+                       // Message message = new Message();
+                        Thread.sleep(100);
+                       if (text.getPaddingLeft() < width && direction==2) {
+                           // message.what = 2;
+                        }
+                        else if(text.getPaddingRight() == 0 && direction == 2){
+                            //message.what = 1;
+                            direction = 1;
+                        }
+                        else if(text.getPaddingLeft() == 0 && direction == 1){
+                            //message.what = 2;
+                            direction = 2;
+                        }
+                        else if(text.getPaddingLeft() < width && direction == 1)
+                        {
+                           // message.what=1;
+                        }
+                        text.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (direction == 1) {
+                                    text.setPadding(text.getPaddingLeft() - 10, text.getPaddingTop(), text.getPaddingRight(), text.getPaddingBottom());
+                                }
+                                if (direction == 2) {
+                                    text.setPadding(text.getPaddingLeft() + 10, text.getPaddingTop(), text.getPaddingRight(), text.getPaddingBottom());
+                                }
+                            }
+                        });
+                    }
+                }catch(InterruptedException e){Thread.currentThread().interrupt();}
+            }
+        });
+            thread.start();
+            On = true;}
+    }
 
-        return super.onOptionsItemSelected(item);
-    }*/
+    private void roll3(){
+        posLeft=text.getPaddingLeft();
+        posRight=text.getPaddingRight();
+        if(On) {On=false;}
+        else{On=true; new RollTask().execute(text.getPaddingLeft(),text.getPaddingRight());}
+    }
+
 }
